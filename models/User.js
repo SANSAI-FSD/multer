de6c6a48282e83222264783c3209c9591f6bb784
SANSAI-FSD <SANSAISANSAI2003@GMAIL.COM>
2025-06-02 15:57:41 +0530

    firet commit

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  biodata: { type: String, required: true },
+  jobRole: { type: String, required: true },
+});
+
+const User = mongoose.model("Post", userSchema);
+
+export default User;
+
